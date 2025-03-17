e85bbbb56ac4eb45cc541dd7863645860fd9492c
Roki K <kiroutp@yandex.ru>
2025-03-17 19:25:00 +0300

    bild the app

--- src/assets/types/cityTextTypes.ts
+++
@@ -1,9 +0,0 @@
-export  enum CityText {
-  SELECT_CITY = "Выберите город",
-  NOT_SELECTED_CITY = "Город не выбран",
-  ADD_CITY = "Добавьте город",
-  CITY_NAME = "Название города",
-  CITY_URL = "Введите URL картинки",
-  CITY_DESCRIPTION = "Описание города",
-  CITY_EXISTS = "Город с таким названием уже есть",
-}
